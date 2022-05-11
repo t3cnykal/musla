49b733f409b5d3e3d8242d1cc02187577dd450fa
Bruno Costa <brunocosta_r@hotmail.com>
2022-05-11 08:04:00 +0100

    Change index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { Fragment } from 'react'
+import { Popover, Transition } from '@headlessui/react'
+import { MenuIcon, XIcon } from '@heroicons/react/outline'
+
+const navigation = [
+  { name: 'Product', href: '#' },
+  { name: 'Features', href: '#' },
+  { name: 'Marketplace', href: '#' },
+  { name: 'Company', href: '#' },
+]
 
 export default function Home() {
   return (
